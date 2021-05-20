cd51b7fd5cd098f9c709856684b60c0dff967b77
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-05-20 11:36:34 +0700

    feat: use continue instead of exit during build debian loop

--- scripts/build-debian.bash
+++ scripts/build-debian.bash
@@ -7,22 +7,20 @@ catkin_generate_changelog --all
 # Do for each ROS 2 packages path
 for PACKAGE in $(colcon list | cut -f2)
 do
-  cd "$PACKAGE" || exit $?
+  cd "$ROOT_DIR" && cd "$PACKAGE" || continue
 
   # Source ROS 2 environment
-  source /opt/ros/$ROS2_DISTRO/setup.bash || exit $?
+  source /opt/ros/$ROS2_DISTRO/setup.bash || continue
 
   # Generate Debian rules
-  bloom-generate rosdebian --ros-distro "$ROS2_DISTRO" || exit $?
+  bloom-generate rosdebian --ros-distro "$ROS2_DISTRO" || continue
 
   # Build package using fakeroot
-  fakeroot debian/rules binary || exit $?
+  fakeroot debian/rules binary || continue
 
   # Install all build result
-  sudo dpkg --install ../*.deb || exit $?
+  sudo dpkg --install ../*.deb || continue
 
   # Move build result to the output directory
   mkdir -p $ROOT_DIR/package && mv ../*.deb $ROOT_DIR/package
-
-  cd $ROOT_DIR || exit $?
 done
