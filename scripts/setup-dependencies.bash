9351608fa494c64fa2d7548a10cfec4830899667
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-05-20 09:43:49 +0700

    fix: dependencies package name

--- scripts/setup-dependencies.bash
+++ scripts/setup-dependencies.bash
@@ -5,7 +5,11 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 # install required tools
-sudo apt update && sudo apt install python3-bloom python3-colcon python3-rosdep fakeroot
+sudo apt update && sudo apt install \
+  python3-bloom \
+  python3-colcon-common-extensions \
+  python3-rosdep \
+  fakeroot
 
 # install ROS 2 project dependencies
 sudo rosdep init && rosdep update && rosdep install -y --rosdistro ${ROS2_DISTRO} --from-paths .
